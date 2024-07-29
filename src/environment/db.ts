a54ae1bc8e00bc2b08192c6077b959eeb8cc37b0
Mariam Ejaz <81904965+Maryam-Ejaz@users.noreply.github.com>
2024-07-29 09:50:29 +0500

    changes again

--- src/environment/db.ts
+++ src/environment/db.ts
@@ -3,5 +3,19 @@ import {TextMessageEntity} from "../domain/entity/text-message-entity";
 import {UserEntity} from "../domain/entity/user-entity";
 
 export const dbDatasourceOptions: DataSourceOptions = {
+    // TODO: replace with your database configuration in the fields bellow:
+    type: "mysql",
+    host: "sql.freedb.tech", //localhost
+    port: 3306,
+    username: "freedb_rooot",
+    password: "gaJ&fhDEC53%t8u",
+    database: "freedb_flutter_chat_app_with_nodejs",
 
+    // No need to change this fields bellow
+    synchronize: true,
+    logging: false,
+    charset : 'utf8mb4',
+    entities: [TextMessageEntity, UserEntity],
+    migrations: [],
+    subscribers: [],
 }
