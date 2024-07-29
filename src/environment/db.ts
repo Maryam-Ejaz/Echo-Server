47cfe22979392eb1ab0437170dfe2489502cdda4
Mariam Ejaz <81904965+Maryam-Ejaz@users.noreply.github.com>
2024-07-29 09:50:06 +0500

    changes again

---
+++ src/environment/db.ts
@@ -0,0 +1,7 @@
+import {DataSourceOptions} from "typeorm/data-source/DataSourceOptions";
+import {TextMessageEntity} from "../domain/entity/text-message-entity";
+import {UserEntity} from "../domain/entity/user-entity";
+
+export const dbDatasourceOptions: DataSourceOptions = {
+
+}
