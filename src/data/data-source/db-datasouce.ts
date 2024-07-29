ded6607b604a8b8c7706ee03224ddcd20e8e5664
Mariam Ejaz <81904965+Maryam-Ejaz@users.noreply.github.com>
2024-07-29 09:41:04 +0500

    changes again

--- src/data/data-source/db-datasouce.ts
+++ src/data/data-source/db-datasouce.ts
@@ -1,5 +1,5 @@
 import "reflect-metadata"
 import { DataSource } from "typeorm"
-import {dbDatasourceOptions} from "./db";
+import {dbDatasourceOptions} from "../../environment/db";
 
 export const AppDataSource = new DataSource(dbDatasourceOptions);
